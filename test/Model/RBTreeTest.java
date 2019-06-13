abb88913b1cdefbb597b60ef33b59a6a506343d3
seckseck <seck3238@hanmail.com>
2019-06-13 13:39:59 +0900

    my commit

--- test/Model/RBTreeTest.java
+++ test/Model/RBTreeTest.java
@@ -12,17 +12,11 @@ public class RBTreeTest {
 	Object data2 ="12";
 	Object data3 ="213";
 	
-	RBNode Main_node = new RBNode(key,data);
-	RBNode Main_node2 = new RBNode(key,data2);
-	RBNode Main_node3 = new RBNode(key,data3);
 
 	RBTree Main_Tree = new RBTree();
 
 	@Test
 	public void test() {
-		Main_Tree.insert(Main_node);
-		Main_Tree.insert(Main_node2);
-		Main_Tree.insert(Main_node3);		
 	}
 
 }
